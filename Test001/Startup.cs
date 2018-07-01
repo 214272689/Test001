7dbbdee8b03095107e74134fac1614be5c7bd9e4
zhangyin <214272689@qq.com>
2018-07-01 17:26:47 +0800

    添加项目文件。

---
+++ Test001/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Test001.Startup))]
+namespace Test001
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
